6f1fd3a989f52a193f28b637acf76120b169cd41
immaonsombi <immanuel.onsombi@strathmore.edu>
2023-11-10 00:41:52 +0300

    more

--- Rest-tutorial/routes/api.php
+++ Rest-tutorial/routes/api.php
@@ -15,13 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('students', [StudentController::class, 'index']);
-Route::post('students', [StudentController::class, 'store']);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::get('students/{id}', [StudentController::class, 'show']);
-Route::get('students/{id}/edit', [StudentController::class, 'edit']);
-Route::put('students/{id}/edit', [StudentController::class, 'update']);
-Route::delete('students/{id}/delete', [StudentController::class, 'destroy']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('students', [StudentController::class, 'index']);
+    Route::post('students', [StudentController::class, 'store']);
+
+    Route::get('students/{id}', [StudentController::class, 'show']);
+    Route::get('students/{id}/edit', [StudentController::class, 'edit']);
+    Route::put('students/{id}/edit', [StudentController::class, 'update']);
+    Route::delete('students/{id}/delete', [StudentController::class, 'destroy']);
+});
